521dfa46127f6f162dd869a8e9c782180b9ab64b
Timo Weiß <info@timo-weiss.com>
2016-04-15 19:59:03 +0200

    add server setup use all config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
 'use strict';
 const Chairo = require('chairo');
-const hapi = require('hapi');
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+server.register({ register: Chairo }, function (err) {
+
+    
+    server.seneca.use('../all');
+
+    server.start(function () {
+        if (err) {
+            throw err;
+        }
+        console.log('Server running at:', server.info.uri);
+    });
+});
