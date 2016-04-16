207cc714d3599fb9d9ab31113de046370526ae1f
Timo Weiß <info@timo-weiss.com>
2016-04-16 18:21:01 +0200

    add rethink changes test

--- user/lib/database.js
+++ user/lib/database.js
@@ -10,12 +10,41 @@ const User = thinky.createModel('User', {
     mail: type.string().email()
 }, {enforce_extra:'remove'});
 
+    
+    User.changes().then(function(feed) {
+        feed.each(function(error, doc) {
+        if (error) {
+            console.log(error);
+            process.exit(1);
+        }
+
+        if (doc.isSaved() === false) {
+            console.log("The following document was deleted:");
+            console.log(doc);
+        }
+            else if (doc.getOldValue() == null) {
+            console.log("A new document was inserted:");
+            console.log(doc);
+            }
+            else {
+            console.log("A document was updated.");
+            console.log("Old value:");
+            console.log(doc.getOldValue());
+            console.log("New value:");
+            console.log(doc);
+            }
+        });
+    });
 
 const createUser = (userData) => {
     if(!userData) {
         throw new Error('spapspsps')
     }
     const user = new User(userData);
+    
+    
+    
+    
     return user.save();
 }
 
