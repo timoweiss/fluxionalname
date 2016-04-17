ea782e7d0a0da91cd4e478bced2b02fc976ef11c
Timo Weiß <info@timo-weiss.com>
2016-04-17 20:47:01 +0200

    add loginUser

--- user/lib/actions.js
+++ user/lib/actions.js
@@ -7,6 +7,7 @@ const SALT_ROUNDS = 10;
 
 module.exports = {
     createUser,
+    loginUser,
     getAllUser
 };
 
@@ -21,6 +22,27 @@ function createUser(args, callback) {
         database.createUser(args)
             .then(data => callback(null, data))
             .catch(callback);
+    });
+}
+
+function loginUser(args, callback) {
+    database.getUserByMail(args.mail).then(user => {
+        if(!user) {
+            return callback({msg: 'user not found'});
+        }
+        console.log('login with:', args.password, user);
+        bcrypt.compare(args.password, user.password, (err, res) => {
+            if(err) {
+                return callback(err);
+            }
+            if (res) {
+                return callback(null, user);
+            }
+            callback({msg: 'wrong password'});
+        }); 
+    }).catch(err => {
+        console.error('login error:',err);
+        callback(err);
     });
 }
 
