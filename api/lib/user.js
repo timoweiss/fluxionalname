1f24401d540cf6ade8ce10ebd89ac2dd23a6bc96
Timo Weiß <info@timo-weiss.com>
2016-04-17 21:27:52 +0200

    init register user

--- api/lib/user.js
+++ api/lib/user.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = {
-    handler
+    handler,
+    registerUser
 };
 
 function handler(request, reply) {
@@ -9,4 +10,8 @@ function handler(request, reply) {
     request.server.seneca.act('role:user,cmd:get,by:nothing,cache$:true', function(err, data) {
         reply(data);
     });
+}
+
+function registerUser(request, reply) {
+    reply();
 }
