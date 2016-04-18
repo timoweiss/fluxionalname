7add4b54330b5bfc2f54ea63526e50fa9e295d29
Timo Weiß <info@timo-weiss.com>
2016-04-18 19:20:44 +0200

    add desc, tags and validations

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,17 +1,25 @@
 'use strict';
 
 const handler = require('../lib/user');
+const validation = require('../validation/userValidation');
 
 module.exports.routes = [{
     method: 'GET',
     path: '/users',
     config: {
+        description: 'deprecated',
+        tags: ['api', 'user'],
         handler: handler.handler
     }
 }, {
     method: 'POST',
     path: '/users',
     config: {
-        handler: handler.registerUser
+        description: 'register new user',
+        tags: ['api', 'user'],
+        handler: handler.registerUser,
+        validate: {
+            payload: validation.register
+        }
     }
 }];
