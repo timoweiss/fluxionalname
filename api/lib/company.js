c7aa288490cda3f45e9b8e1d3c4da36f7cb18091
Timo Weiß <info@timo-weiss.com>
2016-04-18 21:49:11 +0200

    add getCompanyById

--- api/lib/company.js
+++ api/lib/company.js
@@ -1,11 +1,18 @@
 'use strict';
 
 module.exports = {
-    createCompany
+    createCompany,
+    getCompanyById
 };
 
 function createCompany(request, reply) {
     request.server.seneca.act('role:company,cmd:create', request.payload, function(err, data) {
         reply(data);
+    });
+}
+
+function getCompanyById(request, reply) {
+    request.server.seneca.act('role:company,cmd:get,by:id', request.params, function(err, data) {
+        reply(err ||data);
     });
 }
