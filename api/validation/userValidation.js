b41f783e69f9beeda439a5848e679a1c442e8d38
Timo Weiß <info@timo-weiss.com>
2016-04-18 19:20:22 +0200

    fix

--- api/validation/userValidation.js
+++ api/validation/userValidation.js
@@ -7,7 +7,7 @@ let validations = {};
 validations.register = joi.object().keys({
     mail: joi.string().email().min(3).max(60).required()
         .description('Mail address'),
-    password: Joi.string().regex(/[a-zA-Z0-9@#$%_&!"§\/\(\)=\?\^]{3,30}/).required(),
+    password: joi.string().regex(/[a-zA-Z0-9@#$%_&!"§\/\(\)=\?\^]{3,30}/).required(),
     name: joi.string().required(),
     surname: joi.string()
 });
