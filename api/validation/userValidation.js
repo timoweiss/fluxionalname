b7cda38849250fbac20a7a00128b6ef5b6de23fd
Timo Weiß <info@timo-weiss.com>
2016-04-18 19:15:44 +0200

    init

---
+++ api/validation/userValidation.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const joi = require('joi');
+
+let validations = {};
+
+validations.register = joi.object().keys({
+    mail: joi.string().email().min(3).max(60).required()
+        .description('Mail address'),
+    password: Joi.string().regex(/[a-zA-Z0-9@#$%_&!"§\/\(\)=\?\^]{3,30}/).required(),
+    name: joi.string().required(),
+    surname: joi.string()
+});
+
+module.exports = validations;
