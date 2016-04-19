90c6696123c220ed5f5828f30f134bbf084c1f0b
Timo Weiß <info@timo-weiss.com>
2016-04-19 20:04:11 +0200

    impl. selectCompany

--- api/lib/company.js
+++ api/lib/company.js
@@ -2,7 +2,8 @@
 
 module.exports = {
     createCompany,
-    getCompanyById
+    getCompanyById,
+    selectCompany
 };
 
 function createCompany(request, reply) {
@@ -17,6 +18,18 @@ function getCompanyById(request, reply) {
     const pattern = request.applyToDefaults({role: 'company', cmd: 'get', by: 'id'}, request.requesting_user_id);
 
     request.server.seneca.act(pattern, request.params, function(err, data) {
-        reply(err ||data);
+        reply(err || data);
     });
+}
+
+function selectCompany(request, reply) {
+    const sessionData = request.auth.credentials;
+    let old = request.auth.credentials.company_id;
+
+    // TODO validate company_id
+    sessionData.company_id = request.params.id;
+    let newCompanyId = sessionData.company_id;
+
+    request.cookieAuth.set(sessionData);
+    reply({oldCompanyId: old, newCompanyId: newCompanyId});
 }
