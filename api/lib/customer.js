76e01f11ca39f75254b4d8b555f3d921c089528a
Timo Weiß <info@timo-weiss.com>
2016-04-19 22:51:18 +0200

    codestyle

--- api/lib/customer.js
+++ api/lib/customer.js
@@ -6,13 +6,18 @@ module.exports = {
 };
 
 function getCustomer(request, reply) {
-    if(!request.company_id) {
+    if (!request.company_id) {
         return reply(request.unwrap({msg: 'MISSING_COMPANY_ID_SESSION'}));
     }
 
-    console.log(request.requesting_user_id);
-    const pattern = request.applyToDefaults({role: 'customer', cmd: 'get', company_id: request.company_id}, request.requesting_user_id);
+    const pattern = request.applyToDefaults({
+        role: 'customer',
+        cmd: 'get',
+        company_id: request.company_id
+    }, request.requesting_user_id);
+
     request.server.seneca.act(pattern, function (err, customers) {
+
         if (err) {
             // TODO add logging
             console.log(err);
@@ -27,8 +32,15 @@ function getCustomer(request, reply) {
 function createCustomer(request, reply) {
 
     const seneca = request.server.seneca;
-    const pattern = request.applyToDefaults({role: 'customer', cmd: 'create', company_id: request.company_id}, request.requesting_user_id);
+
+    const pattern = request.applyToDefaults({
+        role: 'customer',
+        cmd: 'create',
+        company_id: request.company_id
+    }, request.requesting_user_id);
+
     seneca.act(pattern, request.payload, function (err, customer) {
+
         if (err) {
             // TODO add logging
             console.log(err);
