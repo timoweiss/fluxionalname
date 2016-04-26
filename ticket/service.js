a4b1da02903c57b3e92bb108d08633dee483d014
Timo Weiß <info@timo-weiss.com>
2016-04-26 19:05:03 +0200

    expose updateTicketStatus

--- ticket/service.js
+++ ticket/service.js
@@ -38,12 +38,16 @@ module.exports = function (options) {
         //     customer_id: '321',
         //     labels: ['lalala']
         // }, console.log)
+        // this.act({ticket_id: '571f5cea7e2a114d66e75b7e', role:'ticket', cmd: 'update',what:'status', ruid:'571f5cea7e2a114d66e75b7e', status: 'open'}, function() {
+        //     console.log('looks good?', arguments);
+        // });
 
     });
 
     seneca.add({role: 'ticket', cmd: 'create'}, actions.createTicket);
     seneca.add({role: 'ticket', cmd: 'get', by: 'id'}, actions.getTicketById);
     seneca.add({role: 'ticket', cmd: 'get', by: 'company_id'}, actions.getTicketsByCompanyId);
+    seneca.add({role: 'ticket', cmd: 'update', what: 'status'}, actions.updateTicketStatus);
 
     return {
         name: opts.name
